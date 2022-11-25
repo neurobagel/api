e8926be5f0a86a76a52d68cd28b8168881813900
rmanaem <rmanaem@protonmail.ch>
2022-11-25 12:46:05 -0500

    Implemented basic input test

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,3 +1,8 @@
-def test_get_invalid_sex(test_app):
+def test_get_input(test_app):
+    response = test_app.get("/subjects/?sex=female")
+    assert response.status_code == 200
+
+
+def test_get_invalid_input(test_app):
     response = test_app.get("/subjects/?sex=apple")
     assert response.status_code == 422
