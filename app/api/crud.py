7d462c1dd1101cc2ec31c760142e3d5c624bfbb6
rmanaem <rmanaem@protonmail.ch>
2022-11-28 18:28:15 -0500

    Fixed the deprecated warnings related to httpx.post function "data" parameter is deprecated and replaced with "content"

--- app/api/crud.py
+++ app/api/crud.py
@@ -6,7 +6,7 @@ import httpx
 async def get(sex: str):
     response = httpx.post(
         url=util.QUERY_URL,
-        data=util.create_query(sex=sex),
+        content=util.create_query(sex=sex),
         headers=util.QUERY_HEADER,
         auth=httpx.BasicAuth(
             os.environ.get("USER"), os.environ.get("PASSWORD")
