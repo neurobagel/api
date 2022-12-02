c7b7c00e04595075599a6b217d32362896891d7b
rmanaem <rmanaem@protonmail.ch>
2022-12-02 10:22:17 -0500

    Removed the unused code of create_query function in utility.py
    Co-authored-by: Alyssa Dai <alyssa.ydai@gmail.com>

--- app/api/utility.py
+++ app/api/utility.py
@@ -34,32 +34,14 @@ CATEGORICAL_DOMAINS = [SEX, DIAGNOSIS, IMAGE_MODAL]
 
 
 def create_query(
-    age: tuple = (None, None),
     sex: str = None,
-    image_modal: str = None,
-    diagnosis: str = None,
-    tool: str = None,
-    control: bool = None,
-    num_sessions: int = None,
 ) -> str:
     """Creates a SPARQL query using a query template and adds filters to the query using the input parameters.
 
     Parameters
     ----------
-    age : tuple, optional
-        Subjects' age (upper bound, lower bound), by default (None, None).
     sex : str, optional
         Subjects' sex, by default None.
-    image_modal : str, optional
-        Subjects' imaging modality, by default None.
-    diagnosis : str, optional
-        Subjects' diagnosis, by default None.
-    tool : str, optional
-        Tool, by default None.
-    control : bool, optional
-        Whether the subject was part of the control group, by default None.
-    num_sessions : int, optional
-        Subjects' number of scanning sessions, by default None.
 
     Returns
     -------
@@ -67,49 +49,11 @@ def create_query(
         The SPARQL query.
     """
     subject_level_filters = ""
-    if (
-        isinstance(age, tuple)
-        and not age == (None, None)
-        and not age == ("", "")
-    ):
-        # TODO: revisit this and replace this solution with one that just
-        # doesn't add the filter condition.
-        age = tuple(
-            (
-                default_val if age_val is None else age_val
-                for age_val, default_val in zip(age, [0, 100])
-            )
-        )
-        subject_level_filters += (
-            "\n" + f"FILTER (?{AGE.var} > {age[0]} && ?{AGE.var} < {age[1]})."
-        )
+    session_level_filters = ""
+
     if sex is not None and not sex == "":
         # select_str += f' ?{GENDER_VAR}'
         subject_level_filters += "\n" + f"FILTER (?{SEX.var} = '{sex}')."
-
-    if diagnosis is not None and not diagnosis == "" and not control:
-        # select_str += ' ?diagnosis'
-        subject_level_filters += (
-            "\n" + f"FILTER (?{DIAGNOSIS.var} = <{diagnosis}>)."
-        )
-
-    # TODO: implement the check for control subjects (once we have control
-    # subjects in the meta data)
-    if control is not None and not control == "" and not diagnosis:
-        subject_level_filters += (
-            "\n" + f"FILTER (?{DIAGNOSIS.var} = <{control}>)."
-        )
-
-    if num_sessions is not None:
-        subject_level_filters += (
-            "\n" + f"FILTER (?number_session > {num_sessions})"
-        )
-
-    session_level_filters = ""
-    if image_modal is not None and not image_modal == "":
-        session_level_filters += (
-            "\n" + f"FILTER (?{IMAGE_MODAL.var} = <{image_modal}>)."
-        )
 
     query_template = f"""
     {DEFAULT_CONTEXT}
