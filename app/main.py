33134fba4be2310e003dcada6e27b44c198950e7
rmanaem <rmanaem@protonmail.ch>
2022-12-06 14:02:13 -0500

    Modified main.py module
    - Added a check on startup event to make sure USER and PASSWORD environment variables are defined
    - Moved the get_query path parameter to it's own router
    
    Co-authored-by: Alyssa Dai <alyssa.ydai@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,23 +1,24 @@
 """Define path operations for API."""
 
-from fastapi import FastAPI, Depends
+import os
+
 import uvicorn
-from .api import crud
-from .api.models import QueryModel
+from fastapi import FastAPI
+
+from .api.routers import query
 
 app = FastAPI()
 
 
-@app.get("/query/", tags=["query"])
-async def get_query(query: QueryModel = Depends(QueryModel)):
-    """When a GET request is sent, return list of dicts corresponding to subject-level metadata."""
-    response = await crud.get(query.sex)
-    results = response.json()
-    return [
-        {k: v["value"] for k, v in res.items()}
-        for res in results["results"]["bindings"]
-    ]
+@app.on_event("startup")
+async def auth_check():
+    if os.environ.get("USER") is None or os.environ.get("PASSWORD") is None:
+        raise RuntimeError(
+            "The application was launched and could not find the USER and / or PASSWORD environment variable. Will shut down now."
+        )
 
+
+app.include_router(query.router)
 
 # Automatically start uvicorn server on execution of main.py
 if __name__ == "__main__":
