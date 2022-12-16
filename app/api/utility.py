ee2e5ac2d0a5f1cee730391b8ea6906917f4d4d0
rmanaem <rmanaem@protonmail.ch>
2022-12-16 10:32:41 -0500

    Implemented image modality as an input to create_query function of utility.py module
    - implemented IMAGE_MODAL_CHOICES dictionary
    - create_query now utilizes IMAGE_MODAL_CHOICES to map image_modal input to its corresponding predicate

--- app/api/utility.py
+++ app/api/utility.py
@@ -2,7 +2,7 @@
 
 from collections import namedtuple
 
-# For the request
+# Request constants
 DOG_ROOT = "http://206.12.99.17"
 DOG_DB = "test_data"
 DOG_PORT = 5820
@@ -12,7 +12,7 @@ QUERY_HEADER = {
     "Accept": "application/sparql-results+json",
 }
 
-# For the SPARQL query
+# SPARQL query context
 DEFAULT_CONTEXT = """
 PREFIX bg: <http://neurobagel.org/vocab/>
 PREFIX snomed: <http://neurobagel.org/snomed/>
@@ -32,10 +32,22 @@ CONTROL = Domain("nidm:Control", "nidm:isSubjectGroup")
 
 CATEGORICAL_DOMAINS = [SEX, DIAGNOSIS, IMAGE_MODAL]
 
+# Query choices
+IMAGE_MODAL_CHOICES = {
+    "All": None,
+    "Diffusion weighted": "nidm:DiffusionWeighted",
+    "EEG": "nidm:EEG",
+    "Flow weighted": "nidm:FlowWeighted",
+    "T1 weighted": "nidm:T1Weighted",
+    "T2 weighted": "nidm:T2Weighted",
+}
 
-def create_query(age: tuple = (None, None), sex: str = None) -> str:
+
+def create_query(
+    age: tuple = (None, None), sex: str = None, image_modal: str = None
+) -> str:
     """
-    Creates a SPARQL query using a query template and adds filters to the query using the input parameters.
+    Creates a SPARQL query using a query template and filters it using the input parameters.
 
     Parameters
     ----------
@@ -43,6 +55,8 @@ def create_query(age: tuple = (None, None), sex: str = None) -> str:
         Minimum and maximum age of subject, by default (None, None).
     sex : str, optional
         Subject sex, by default None.
+    image_modal: str, optional
+        Imaging modality of subject scans, by default None.
 
     Returns
     -------
@@ -62,6 +76,12 @@ def create_query(age: tuple = (None, None), sex: str = None) -> str:
         subject_level_filters += "\n" + f"FILTER (?{SEX.var} = '{sex}')."
 
     session_level_filters = ""
+
+    if image_modal is not None and image_modal != "All":
+        session_level_filters += (
+            "\n"
+            + f"FILTER (?{IMAGE_MODAL.var} = {IMAGE_MODAL_CHOICES[image_modal]})."
+        )
 
     query_template = f"""
     {DEFAULT_CONTEXT}
