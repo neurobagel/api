6db8f7c430a2cba93cd9b1c6f37955e5f8f2f9aa
rmanaem <rmanaem@protonmail.ch>
2023-02-15 01:16:28 -0500

    Added cors middleware to the api

--- app/main.py
+++ app/main.py
@@ -4,11 +4,20 @@ import os
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 
 from .api.routers import query
 
 app = FastAPI(default_response_class=ORJSONResponse)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
