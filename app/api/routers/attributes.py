716d6a88e1a2aa9daaa71714f7d8897fb2a119a7
Alyssa Dai <alyssa.ydai@gmail.com>
2023-10-02 14:21:46 -0400

    create router and GET path operation for fetching attribute classes

---
+++ app/api/routers/attributes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from .. import crud
+
+router = APIRouter(prefix="/attributes", tags=["attributes"])
+
+
+@router.get("/", response_model=dict)
+async def get_attributes():
+    """When a GET request is sent, return a dict containing a list of the harmonized controlled term attributes."""
+    response = await crud.get_controlled_term_attributes()
+
+    return response
