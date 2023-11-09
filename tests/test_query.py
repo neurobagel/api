1b44ae04384b4ce43ae351182dbedcd8fb8fa847
Alyssa Dai <alyssa.ydai@gmail.com>
2023-11-09 16:33:37 -0500

    add integration tests for available terms endpoint

--- tests/test_query.py
+++ tests/test_query.py
@@ -496,6 +496,115 @@ def test_get_terms_invalid_data_element_URI(
     assert response.status_code == 422
 
 
+def test_get_terms_for_attribute_with_vocab_lookup(test_app, monkeypatch):
+    """
+    Given a valid data element URI with a vocabulary lookup file available, returns prefixed term URIs and their human-readable labels (where found)
+    for instances of that data element, and excludes terms with unrecognized namespaces with an informative warning.
+    """
+    monkeypatch.setenv(util.GRAPH_USERNAME.name, "SomeUser")
+    monkeypatch.setenv(util.GRAPH_PASSWORD.name, "SomePassword")
+
+    # TODO: Since these mock HTTPX response JSONs are so similar (and useful), refactor their creation out into a function.
+    mock_response_json = {
+        "head": {"vars": ["termURL"]},
+        "results": {
+            "bindings": [
+                {
+                    "termURL": {
+                        "type": "uri",
+                        "value": "https://www.cognitiveatlas.org/task/id/tsk_U9gDp8utahAfO",
+                    }
+                },
+                {
+                    "termURL": {
+                        "type": "uri",
+                        "value": "https://www.cognitiveatlas.org/task/id/not_found_id",
+                    }
+                },
+                {
+                    "termURL": {
+                        "type": "uri",
+                        "value": "https://www.notanatlas.org/task/id/tsk_alz5hjlUXp4WY",
+                    }
+                },
+            ]
+        },
+    }
+
+    def mock_httpx_post(**kwargs):
+        return httpx.Response(status_code=200, json=mock_response_json)
+
+    monkeypatch.setattr(httpx, "post", mock_httpx_post)
+
+    with pytest.warns(
+        UserWarning,
+        match="does not come from a vocabulary recognized by Neurobagel",
+    ):
+        with test_app:
+            response = test_app.get("/attributes/nb:Assessment")
+
+    assert response.json() == {
+        "nb:Assessment": [
+            {
+                "TermURL": "cogatlas:tsk_U9gDp8utahAfO",
+                "Label": "Pittsburgh Stress Battery",
+            },
+            {"TermURL": "cogatlas:not_found_id", "Label": None},
+        ]
+    }
+
+
+def test_get_terms_for_attribute_without_vocab_lookup(test_app, monkeypatch):
+    """
+    Given a valid data element URI without any vocabulary lookup file available, returns prefixed term URIs and  instances of that data element, and excludes terms with unrecognized namespaces with an informative warning.
+    """
+    monkeypatch.setenv(util.GRAPH_USERNAME.name, "SomeUser")
+    monkeypatch.setenv(util.GRAPH_PASSWORD.name, "SomePassword")
+    # Create a mock context with some fake ontologies (with no vocabulary lookup files)
+    monkeypatch.setattr(
+        util,
+        "CONTEXT",
+        {
+            "cko": "https://www.coolknownontology.org/task/id/",
+            "ako": "https://www.awesomeknownontology.org/vocab/",
+        },
+    )
+
+    mock_response_json = {
+        "head": {"vars": ["termURL"]},
+        "results": {
+            "bindings": [
+                {
+                    "termURL": {
+                        "type": "uri",
+                        "value": "https://www.coolknownontology.org/task/id/trm_123",
+                    }
+                },
+                {
+                    "termURL": {
+                        "type": "uri",
+                        "value": "https://www.coolknownontology.org/task/id/trm_234",
+                    }
+                },
+            ]
+        },
+    }
+
+    def mock_httpx_post(**kwargs):
+        return httpx.Response(status_code=200, json=mock_response_json)
+
+    monkeypatch.setattr(httpx, "post", mock_httpx_post)
+
+    response = test_app.get("/attributes/nb:SomeClass")
+
+    assert response.json() == {
+        "nb:SomeClass": [
+            {"TermURL": "cko:trm_123", "Label": None},
+            {"TermURL": "cko:trm_234", "Label": None},
+        ]
+    }
+
+
 def test_get_attributes(
     test_app,
     monkeypatch,
