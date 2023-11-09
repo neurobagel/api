ce97d5f80595ddae080cc72454a1002c19d5b3d6
Alyssa Dai <alyssa.ydai@gmail.com>
2023-11-09 14:23:37 -0500

    add util functions for checking and stripping term namespaces

--- app/api/utility.py
+++ app/api/utility.py
@@ -251,6 +251,53 @@ def create_terms_query(data_element_URI: str) -> str:
     return "\n".join([create_context(), query_string])
 
 
+def is_term_namespace_in_context(term_url: str) -> bool:
+    """
+    Performs basic check for if a term URL contains a namespace URI from the context.
+
+    Parameters
+    ----------
+    url : str
+        A controlled term URI.
+
+    Returns
+    -------
+    bool
+        True if the term URL contains a namespace URI from the context, False otherwise.
+    """
+    for uri in CONTEXT.values():
+        if uri in term_url:
+            return True
+    return False
+
+
+def strip_namespace_from_term_uri(term: str, has_prefix: bool = False) -> str:
+    """
+    Removes namespace URI or prefix from a term URI if the namespace is recognized.
+
+    Parameters
+    ----------
+    uri : str
+        A controlled term URI.
+
+    Returns
+    -------
+    str
+        The unique term ID.
+    """
+    if has_prefix:
+        for prefix in CONTEXT:
+            if prefix in term:
+                return term.replace(prefix, "")
+    elif not has_prefix:
+        for uri in CONTEXT.values():
+            if uri in term:
+                return term.replace(uri, "")
+
+    # If no match found within the context, return original term
+    return term
+
+
 def replace_namespace_uri(url: str) -> str:
     """
     Replaces namespace URIs in term URLs with corresponding prefixes from the context.
