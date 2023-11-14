af9e0d6e95bfc6efe8617364c1f003b8ddf068b1
Alyssa Dai <alyssa.ydai@gmail.com>
2023-11-14 14:23:20 -0500

    test new httpx response unpacking util

---
+++ tests/test_utility.py
@@ -0,0 +1,61 @@
+"""Test utility functions."""
+from app.api import utility as util
+
+
+def test_unpack_http_response_json_to_dicts():
+    """Test that given a valid httpx JSON response, the function returns a simplified list of dicts with the correct keys and values."""
+    mock_response_json = {
+        "head": {"vars": ["dataset_uuid", "total_subjects"]},
+        "results": {
+            "bindings": [
+                {
+                    "dataset_uuid": {
+                        "type": "uri",
+                        "value": "http://neurobagel.org/vocab/ds1234",
+                    },
+                    "total_subjects": {
+                        "datatype": "http://www.w3.org/2001/XMLSchema#integer",
+                        "type": "literal",
+                        "value": "70",
+                    },
+                },
+                {
+                    "dataset_uuid": {
+                        "type": "uri",
+                        "value": "http://neurobagel.org/vocab/ds2345",
+                    },
+                    "total_subjects": {
+                        "datatype": "http://www.w3.org/2001/XMLSchema#integer",
+                        "type": "literal",
+                        "value": "40",
+                    },
+                },
+                {
+                    "dataset_uuid": {
+                        "type": "uri",
+                        "value": "http://neurobagel.org/vocab/ds3456",
+                    },
+                    "total_subjects": {
+                        "datatype": "http://www.w3.org/2001/XMLSchema#integer",
+                        "type": "literal",
+                        "value": "84",
+                    },
+                },
+            ]
+        },
+    }
+
+    assert util.unpack_http_response_json_to_dicts(mock_response_json) == [
+        {
+            "dataset_uuid": "http://neurobagel.org/vocab/ds1234",
+            "total_subjects": "70",
+        },
+        {
+            "dataset_uuid": "http://neurobagel.org/vocab/ds2345",
+            "total_subjects": "40",
+        },
+        {
+            "dataset_uuid": "http://neurobagel.org/vocab/ds3456",
+            "total_subjects": "84",
+        },
+    ]
