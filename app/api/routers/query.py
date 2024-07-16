a89496c81a91a090a4c788cc3dce0ace83e6f15a
Alyssa Dai <alyssa.ydai@gmail.com>
2024-07-15 22:11:30 -0400

    add implicit OAuth flow + Google token verification for /query route

--- app/api/routers/query.py
+++ app/api/routers/query.py
@@ -2,17 +2,41 @@
 
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2
 
-from .. import crud
+from .. import crud, security
 from ..models import CohortQueryResponse, QueryModel
+from ..security import verify_token
 
 router = APIRouter(prefix="/query", tags=["query"])
 
+# Adapted from info in https://github.com/tiangolo/fastapi/discussions/9137#discussioncomment-5157382
+oauth2_scheme = OAuth2(
+    flows={
+        "implicit": {
+            "authorizationUrl": "https://accounts.google.com/o/oauth2/auth",
+        }
+    },
+    # Don't automatically error out when request is not authenticated, to support optional authentication
+    auto_error=False,
+)
+
 
 @router.get("/", response_model=List[CohortQueryResponse])
-async def get_query(query: QueryModel = Depends(QueryModel)):
+async def get_query(
+    query: QueryModel = Depends(QueryModel),
+    token: str | None = Depends(oauth2_scheme),
+):
     """When a GET request is sent, return list of dicts corresponding to subject-level metadata aggregated by dataset."""
+    if security.AUTH_ENABLED:
+        if token is None:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Not authenticated",
+            )
+        verify_token(token)
+
     response = await crud.get(
         query.min_age,
         query.max_age,
