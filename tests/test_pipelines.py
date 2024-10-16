e1f8d0628017eccba25fd0b97c046aec30b3590b
Alyssa Dai <alyssa.ydai@gmail.com>
2024-10-15 22:47:36 -0400

    refactor test & fix docstring

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -7,24 +7,32 @@ def test_get_pipeline_versions_response(
     test_app, monkeypatch, set_test_credentials
 ):
     """
-    Given a request to /attributes/nb:Pipeline/{resource}/versions with a valid pipeline name,
+    Given a request to /pipelines/{pipeline_term}/versions with a valid pipeline name,
     returns a dict where the key is the pipeline resource and the value is a list of pipeline versions.
     """
-    mock_graph_response = {
-        "head": {"vars": ["pipeline_version"]},
-        "results": {
-            "bindings": [
-                {"pipeline_version": {"type": "literal", "value": "23.1.3"}},
-                {"pipeline_version": {"type": "literal", "value": "20.2.7"}},
-            ]
-        },
-    }
-    # NOTE: We get away with a single param lambda func here because the API route function
-    # (attributes.get_pipeline_versions) that calls post_query_to_graph uses the default timeout,
-    # so we don't need to worry about that extra parameter when mocking
-    monkeypatch.setattr(
-        crud, "post_query_to_graph", lambda x: mock_graph_response
-    )
+
+    def mock_post_query_to_graph(query, timeout=5.0):
+        return {
+            "head": {"vars": ["pipeline_version"]},
+            "results": {
+                "bindings": [
+                    {
+                        "pipeline_version": {
+                            "type": "literal",
+                            "value": "23.1.3",
+                        }
+                    },
+                    {
+                        "pipeline_version": {
+                            "type": "literal",
+                            "value": "20.2.7",
+                        }
+                    },
+                ]
+            },
+        }
+
+    monkeypatch.setattr(crud, "post_query_to_graph", mock_post_query_to_graph)
 
     response = test_app.get(f"{BASE_ROUTE}/np:fmriprep/versions")
     assert response.status_code == 200
