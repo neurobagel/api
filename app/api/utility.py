c465e7aa73504f64ffc35b1740c67971cce3937d
Alyssa Dai <alyssa.ydai@gmail.com>
2025-03-26 16:50:28 -0400

    [MNT] Update diagnosis terms to SNOMED v20250227 and standardize vocab file format (#427)
    * update SNOMED CT disorders list to v20250227
    
    * standardize snomedct vocab file names
    
    * rename vocab file

--- app/api/utility.py
+++ app/api/utility.py
@@ -37,10 +37,6 @@ CATEGORICAL_DOMAINS = [SEX, DIAGNOSIS, IMAGE_MODAL, ASSESSMENT]
 
 IS_CONTROL_TERM = "ncit:C94342"
 
-BACKUP_VOCAB_DIR = (
-    Path(__file__).absolute().parents[2] / "vocab/backup_external"
-)
-
 
 def parse_origins_as_list(allowed_origins: str | None) -> list:
     """Returns user-defined allowed origins as a list."""
@@ -414,43 +410,28 @@ def load_json(path: Path) -> dict:
         return json.load(f)
 
 
-def create_snomed_assessment_lookup(output_path: Path):
+def reformat_snomed_terms_for_lookup(
+    input_terms_path: Path, output_terms_path: Path
+):
     """
-    Reads in a file of assessment terms from the SNOMED vocabulary and writes term ID-label mappings to a temporary lookup file.
+    Reads in a file containing terms from the SNOMED CT vocabulary, strips the SNOMED prefix from term URIS,
+    and writes the resulting term ID-label mappings to a temporary lookup file.
 
     Saves a JSON with keys corresponding to SNOMED IDs and values corresponding to human-readable term names.
 
     Parameters
     ----------
-    output_path : Path
+    lookup_path : Path
         File path to store output vocabulary lookup file.
     """
-    vocab = load_json(BACKUP_VOCAB_DIR / "snomed_assessment.json")
+    vocab = load_json(input_terms_path)
 
     term_labels = {
         term["identifier"].removeprefix("snomed:"): term["label"]
         for term in vocab
     }
 
-    with open(output_path, "w") as f:
-        f.write(json.dumps(term_labels, indent=2))
-
-
-def create_snomed_disorder_lookup(output_path: Path):
-    """
-    Reads in a file of disorder terms from the SNOMED CT vocabulary and writes term ID-label mappings to a temporary lookup file.
-
-    Saves a JSON with keys corresponding to SNOMED CT IDs and values corresponding to human-readable term names.
-
-    Parameters
-    ----------
-    output_path : Path
-        File path to store output vocabulary lookup file.
-    """
-    vocab = load_json(BACKUP_VOCAB_DIR / "snomedct_disorder.json")
-
-    term_labels = {term["sctid"]: term["preferred_name"] for term in vocab}
-    with open(output_path, "w") as f:
+    with open(output_terms_path, "w") as f:
         f.write(json.dumps(term_labels, indent=2))
 
 
--- app/main.py
+++ app/main.py
@@ -16,6 +16,10 @@ from .api.config import Settings, settings
 from .api.routers import assessments, attributes, diagnoses, pipelines, query
 from .api.security import check_client_id
 
+BACKUP_VOCAB_DIR = (
+    Path(__file__).absolute().parents[1] / "vocab/backup_external"
+)
+
 
 def validate_environment_variables():
     """
@@ -63,11 +67,13 @@ def initialize_vocabularies():
         / "snomedct_disorder_term_labels.json",
     }
 
-    util.create_snomed_assessment_lookup(
-        app.state.vocab_lookup_paths["snomed_assessment"]
+    util.reformat_snomed_terms_for_lookup(
+        input_terms_path=BACKUP_VOCAB_DIR / "snomedct_assessment.json",
+        output_terms_path=app.state.vocab_lookup_paths["snomed_assessment"],
     )
-    util.create_snomed_disorder_lookup(
-        app.state.vocab_lookup_paths["snomed_disorder"]
+    util.reformat_snomed_terms_for_lookup(
+        input_terms_path=BACKUP_VOCAB_DIR / "snomedct_disorder.json",
+        output_terms_path=app.state.vocab_lookup_paths["snomed_disorder"],
     )
 
 
